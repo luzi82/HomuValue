c484cc3f8ffffc2019ff684f5b8a4f9f3ed00b54
Luzi82 <luzi82@gmail.com>
2013-12-14 12:57:31 +0800

    add sum on var test

--- HomuValueTest/src/com/luzi82/homuvalue/test/T001_IntegerValue.java
+++ HomuValueTest/src/com/luzi82/homuvalue/test/T001_IntegerValue.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import com.luzi82.homuvalue.IntegerValue;
 import com.luzi82.homuvalue.Value;
+import com.luzi82.homuvalue.Value.Variable;
 
 public class T001_IntegerValue {
 
@@ -15,10 +16,23 @@ public class T001_IntegerValue {
 
 		Value<Integer> i7 = IntegerValue.sum(i3, i4);
 		Assert.assertEquals((Integer) 7, i7.get());
-		
+
 		@SuppressWarnings("unchecked")
 		Value<Integer> i10 = IntegerValue.sum(new Value[] { i3, i3, i4 });
 		Assert.assertEquals((Integer) 10, i10.get());
+	}
+
+	@Test
+	public void sumVar() {
+		Variable<Integer> i3 = Value.variable(3);
+		Variable<Integer> i4 = Value.variable(4);
+
+		Value<Integer> i7 = IntegerValue.sum(i3, i4);
+		Assert.assertEquals((Integer) 7, i7.get());
+
+		i3.set(9);
+
+		Assert.assertEquals((Integer) 13, i7.get());
 	}
 
 }
