5a69653733a5503c22888c74e95923d7c88100ae
Luzi82 <luzi82@gmail.com>
2015-01-31 12:02:41 +0800

    fix test

--- HomuValueTest/src/com/luzi82/homuvalue/test/T003_Remote.java
+++ HomuValueTest/src/com/luzi82/homuvalue/test/T003_Remote.java
@@ -4,47 +4,8 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import com.luzi82.homuvalue.AbstractVariable;
-import com.luzi82.homuvalue.RemoteGroup;
 
 public class T003_Remote {
-
-	@Test
-	public void t0() {
-		class I {
-			public int i = 0;
-		}
-
-		class T extends RemoteGroup<I> {
-			public final AbstractVariable<Integer> i;
-
-			public T(I aV) {
-				super(aV);
-				i = new AbstractVariable<Integer>() {
-					@Override
-					protected void variableSet(Integer t) {
-						iV.i = t;
-					}
-
-					@Override
-					protected Integer variableGet() {
-						return iV.i;
-					}
-				};
-				addChild(i);
-			}
-		}
-
-		I i = new I();
-		T t = new T(i);
-		t.get();
-
-		TestListener<I> l = new TestListener<I>();
-		t.addListener(l);
-
-		t.i.set(1);
-
-		Assert.assertNotNull(l.v);
-	}
 
 	@Test
 	public void t1() {
